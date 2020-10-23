9e8c94460e4b79f6325ac2ec481b417f86eab021
soo2181 <44142352+soo2181@users.noreply.github.com>
2020-10-23 22:51:21 +0900

    Program modified on GitHub

--- HelloWorld/helloworld.cpp
+++ HelloWorld/helloworld.cpp
@@ -1,12 +1,12 @@
-#include <iostream>      //해더파일
-using namespace std;     //네임스페이스
-
-int main(void)           //메인함수
+#include <iostream>
+using namespace std;
+int main(void)
 {
-    cout << "Hello World" << endl; //문자열 출력
+    cout << "Hello World" << endl;
     cout << "Hello my World" << endl;
     cout << "World is mine" << endl;
     cout << "Luna is mine" << endl;
     cout << "Earth is mine" << endl;
-    return 0; // 프로그램 끝
+    cout << "World is Github" << endl;
+    return 0;
 }
