6c701f5714656b6e23473da11e06ebf31c954080
soo2181 <soo2181@gmail.com>
2020-10-23 23:21:43 +0900

    merge_commit

--- HelloWorld/helloworld.cpp
+++ HelloWorld/helloworld.cpp
@@ -6,5 +6,7 @@ int main(void)           //메인함수
     cout << "Hello World" << endl; //문자열 출력
     cout << "Hello my World" << endl;
     cout << "World is mine" << endl;
+    cout << "Luna is mine" << endl;
+    cout << "Earth on Local repository" << endl;
     return 0; // 프로그램 끝
 }
