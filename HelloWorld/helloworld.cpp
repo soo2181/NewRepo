d12a80ce482130ab83f6a192d7888be8b3080bfb
soo2181 <soo2181@gmail.com>
2020-10-23 23:49:38 +0900

    local repository delete

--- HelloWorld/helloworld.cpp
+++ HelloWorld/helloworld.cpp
@@ -6,7 +6,6 @@ int main(void)
     cout << "Hello World" << endl;
     cout << "Hello my World" << endl;
     cout << "World is mine" << endl;
-    cout << "Luna is mine" << endl;
     cout << "Earth on Local repository" << endl;
     return 0;
 }
